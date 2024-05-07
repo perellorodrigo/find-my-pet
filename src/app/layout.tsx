4a37e4de2e578a4a1b47a7af499e3911905b2a5f
Rodrigo Perello <penellorodi@gmail.com>
2024-05-06 22:47:56 -0300

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-
+import { Analytics } from "@vercel/analytics/react";
 const fontSans = FontSans({
 	subsets: ["latin"],
 	variable: "--font-sans",
@@ -28,6 +28,7 @@ export default function RootLayout({
 			>
 				{children}
 			</body>
+			<Analytics />
 		</html>
 	);
 }
