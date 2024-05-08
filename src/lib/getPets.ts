012acb08da76bb916108a22f0da1d41f6c0b4fd3
Rodrigo Perello <penellorodi@gmail.com>
2024-05-08 01:26:59 -0300

    try to disable cache store for kv

--- src/lib/getPets.ts
+++ src/lib/getPets.ts
@@ -6,8 +6,17 @@ import {
 	filterableFields,
 } from "@/lib/types";
 import { createClient } from "contentful";
-import { kv } from "@vercel/kv";
-import { NextRequest } from "next/server";
+import { createClient as createKVClient } from "@vercel/kv";
+
+if (!process.env.KV_REST_API_TOKEN || !process.env.KV_REST_API_URL) {
+	throw new Error("Missing KV API keys");
+}
+
+const kv = createKVClient({
+	token: process.env.KV_REST_API_TOKEN,
+	url: process.env.KV_REST_API_URL,
+	cache: "no-store",
+});
 
 export type Filter = {
 	[key in FilterableField]?: string[];
@@ -58,22 +67,14 @@ export async function getPets({
 
 	const cacheKey = JSON.stringify(queryObj);
 
+	// Does this write to cache vercel?
 	const cached = await kv.get<PetResponse>(cacheKey);
 	if (cached) {
 		console.log("Cache hit for", cacheKey);
 		return cached;
 	}
 
-	const results = await client.getEntries<PetSkeleton>({
-		content_type: "pet",
-		"fields.gender[in]": getFilterValue(filters?.gender),
-		"fields.species[in]": getFilterValue(filters?.species),
-		"fields.breed[in]": getFilterValue(filters?.breed),
-		"fields.size[in]": getFilterValue(filters?.size),
-		"fields.color[in]": getFilterValue(filters?.color),
-		skip: skip || 0,
-		query: searchTerm,
-	});
+	const results = await client.getEntries<PetSkeleton>(queryObj);
 
 	const returnObj = {
 		items: results.items,
@@ -84,7 +85,7 @@ export async function getPets({
 
 	console.log("Setting cache for", cacheKey);
 	await kv.set(cacheKey, JSON.stringify(returnObj), {
-		ex: 60 * 60 * 3, // 1 hour
+		ex: 60 * 60 * 3, // 3 hours
 	});
 
 	return returnObj;
